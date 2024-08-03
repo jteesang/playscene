ae96b7293116774fe5c5f5815155a2487ea139b5
Julia Tseng <juliatseng12345@gmail.com>
2024-08-03 15:46:02 -0700

    Center UI dialog box and panel

--- app/page.js
+++ app/page.js
@@ -148,11 +148,11 @@ export default function Home() {
             transition
             className="fixed inset-0 bg-light-gray bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
           />
-          <div className="fixed inset-0 w-screen overflow-y-auto">
-            <div className="flex min-h-full items-end justify-center text-center sm:items-center sm:p-0">
+          <div className="fixed inset-0 w-full content-center inset-0 w-screen overflow-y-auto">
+            <div className="flex w-full justify-center text-center sm:items-center sm:p-0">
               <DialogPanel
                 transition
-                className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+                className="relative transform overflow-hidden rounded-md text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
               >
               <div className="bg-light-black px-4 pb-8 pt-5 sm:p-6 sm:pb-8">
                 { playlist === '' || playlist === null ?
